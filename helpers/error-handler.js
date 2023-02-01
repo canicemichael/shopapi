0a849b506ebffaf3e7be5a3202a561f59419f6d8
canicemiachael <canicemiachael@gmail.com>
2023-02-01 05:55:49 +0100

    Update-error-handler

--- helpers/error-handler.js
+++ helpers/error-handler.js
@@ -12,7 +12,7 @@ function errorHandler(err, req, res, next) {
   }
 
   // default to 500 server error
-  return res.status(500).json(err);
+  return res.status(500).json({ message: "Internal server error" });
 }
 
 module.exports = { errorHandler };
